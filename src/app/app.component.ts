47084687104c090f78ea0d898d9d135b0a70a3d7
Patrick Brunner <patrick.brunner@inf.unibe.ch>
2022-04-21 08:51:27 +0200

    added homepage component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'OSA-eignungstool';
+  currentPage = 'Startseite';
+  progressPercent = new BehaviorSubject<number>(0)
+
+  ngOnInit() {
+    this.progressPercent.subscribe(progress => {
+      const element = document.getElementById('progress-bar-fill')
+      if (element){
+        element.style.width = `${progress}%`
+      }
+    })
+  }
+
+  add(amount: number) {
+    let val = this.progressPercent.value + amount
+    if (val > 100) {
+      this.progressPercent.next(100)
+    } else {
+      this.progressPercent.next(val)
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {Route, RouterModule} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+
+const routes: Route[] = [
+  { path: 'home', component: HomeComponent }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
