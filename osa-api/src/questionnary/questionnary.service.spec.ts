2ac5e6935440f72f0a60a82961b2f97bc67c183c
Lena Georgescu <lena.georgescu@bfh.ch>
2022-05-06 14:51:59 +0200

    Added a test for questionnary.service

--- osa-api/src/questionnary/questionnary.service.spec.ts
+++ osa-api/src/questionnary/questionnary.service.spec.ts
@@ -1,18 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MongoModule } from '../mongo/mongo.module';
+import { MongoService } from '../mongo/mongo.service';
 import { QuestionnaryService } from './questionnary.service';
 
 describe('QuestionnaryService', () => {
   let service: QuestionnaryService;
+  let mongo: MongoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [QuestionnaryService],
+      imports: [MongoModule],
+      providers: [
+        QuestionnaryService,
+        { provide: MongoService, useClass: MongoService },
+      ],
     }).compile();
 
     service = module.get<QuestionnaryService>(QuestionnaryService);
+    mongo = module.get<MongoService>(MongoService)
   });
+  
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it ('service should call mongoService getCollection', async() =>{
+    const spy = jest.spyOn (mongo, 'getCollection');
+    await service.findAll();
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+
+
+
+
 });
