e8894e834ce36d18b9cf02dc7e95c546e06c525f
Lena Georgescu <lena.georgescu@bfh.ch>
2022-05-13 12:37:25 +0200

    added seed.service with tests

--- osa-api/src/seed/seed.service.spec.ts
+++ osa-api/src/seed/seed.service.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SeedService } from './seed.service';
+import { MongoService} from '../mongo/mongo.service'
+import { async } from 'rxjs';
 
 describe('SeedService', () => {
   let service: SeedService;
+  let mongo: MongoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SeedService],
+      providers: [SeedService, 
+        {
+          provide: MongoService,
+          useFactory: async () => {
+            const mongo = new MongoService();
+            await mongo.onModuleInit();
+            return mongo;
+          },
+        },
+      ],
     }).compile();
-
     service = module.get<SeedService>(SeedService);
+    mongo = await module.resolve<MongoService>(MongoService);
+  });
+
+  afterEach(async() => {
+      await mongo.onModuleDestroy();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it ('should create questionnaire collection on ApplicationBootstrap', async () => {
+    const COLLECTIONNAME = 'osa-fragebogen'
+    const spy = jest.spyOn (mongo, 'createCollection');
+    await service.onApplicationBootstrap();
+    expect (spy).toBeCalledTimes(1);
+    const collection = await mongo.getCollection(COLLECTIONNAME);
+    expect (await collection.countDocuments()).toBe(4)
+  }
+  )
 });
