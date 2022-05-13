fb7f2b7f6fff6b2b4226bfb24372452759e80b33
Lena Georgescu <lena.georgescu@bfh.ch>
2022-05-13 14:28:30 +0200

    corrected dependencies

--- osa-api/src/app.module.ts
+++ osa-api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { QuestionnaireModule } from './questionnaire/questionnaire.module';
 import { SeedService } from './seed/seed.service';
 import { SeedModule } from './seed/seed.module';
+import { MongoModule } from './mongo/mongo.module';
 
 @Module({
-  imports: [QuestionnaireModule, SeedModule],
+  imports: [QuestionnaireModule, SeedModule, MongoModule],
   controllers: [AppController],
   providers: [AppService, SeedService],
 })
