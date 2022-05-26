ae26feaca9627f7505fb0e86449cd639cb946c80
Lena Georgescu <lena.georgescu@bfh.ch>
2022-05-26 19:21:14 +0200

    fixed dependencies in questionnaire controller

--- osa-api/src/questionnaire/questionnaire.controller.spec.ts
+++ osa-api/src/questionnaire/questionnaire.controller.spec.ts
@@ -1,20 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MongoService } from '../mongo/mongo.service';
 import { QuestionnaireController } from './questionnaire.controller';
 import { QuestionnaireService } from './questionnaire.service';
 
 describe('QuestionnaireController', () => {
   let controller: QuestionnaireController;
+  let mongo: MongoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [QuestionnaireController],
-      providers: [QuestionnaireService],
+      providers: [ QuestionnaireService, 
+        { provide: MongoService,
+          useFactory: async () => {
+          let mongo = new MongoService();
+          mongo.onModuleInit();
+          return mongo;
+        },
+      },
+      ],
     }).compile();
-
     controller = module.get<QuestionnaireController>(QuestionnaireController);
+    mongo = await module.resolve<MongoService>(MongoService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+  
+
 });
--- osa-api/src/questionnaire/questionnaire.service.spec.ts
+++ osa-api/src/questionnaire/questionnaire.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { Collection, ObjectId} from 'mongodb';
+import { ObjectId} from 'mongodb';
 import { MongoService } from '../mongo/mongo.service';
 import { QuestionnaireService } from './questionnaire.service';
 
