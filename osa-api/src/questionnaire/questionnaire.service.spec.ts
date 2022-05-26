6e58970be2ff7ef8cf3947b5ff85db9b51e578f7
Lena Georgescu <lena.georgescu@bfh.ch>
2022-05-26 18:34:07 +0200

    added more questionnaire service tests

--- osa-api/src/questionnaire/questionnaire.service.spec.ts
+++ osa-api/src/questionnaire/questionnaire.service.spec.ts
@@ -1,11 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ObjectId} from 'mongodb';
+import { Collection, ObjectId} from 'mongodb';
 import { MongoService } from '../mongo/mongo.service';
 import { QuestionnaireService } from './questionnaire.service';
 
 describe('QuestionnaireService', () => {
   let service: QuestionnaireService;
   let mongo: MongoService;
+  const COLLECTIONNAME = "osa-fragebogen";
   
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,6 +15,8 @@ describe('QuestionnaireService', () => {
           useFactory: async () => {
           let mongo = new MongoService();
           await mongo.onModuleInit();
+          const COLLECTIONNAME = "osa-fragebogen";
+          await mongo.createCollection(COLLECTIONNAME);
           return mongo;
         },
       },
@@ -22,26 +25,42 @@ describe('QuestionnaireService', () => {
 
     service = module.get<QuestionnaireService>(QuestionnaireService);
     mongo = await module.resolve<MongoService>(MongoService);
-    
   });
 
   afterEach(async () => {
     await mongo.onModuleDestroy();
   });
   
-
+  describe ('should be defined', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
   it('mongo should be defined', () => {
-    expect(mongo).toBeDefined();
+    expect(mongo).toBeUndefined;
   });
+  })
 
-  it ('service should call mongoService getCollection', async() =>{
+  describe ('service should call mongoService getCollection', () => {
+  it ('findOne should call mongoService getCollection', async () => {
     const spy = jest.spyOn (mongo, 'getCollection');
-    await service.findAll();
+    await service.findOne("507f1f77bcf86cd799439011");
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
+  it ('create should call mongoService getCollection', async () => {
+    const spy = jest.spyOn (mongo, 'getCollection');
+    await service.create({});
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it ('update should call mongoService getCollection', async () => {
+    let collection = mongo.getCollection(COLLECTIONNAME);
+    let id = new ObjectId("507f1f77bcf86cd799439011");
+    collection.insertOne({_id: id});
+    const spy = jest.spyOn (mongo, 'getCollection');
+    await service.update("507f1f77bcf86cd799439011",{"age": 23});
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+  })
 });
